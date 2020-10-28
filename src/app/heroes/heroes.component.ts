f5aaae1af75f83dd6ef0c30fe9e13e289dfa8a63
Wenrui Xu <tigerwxu@gmail.com>
2020-10-29 11:41:09 +1300

    Add initial commit
    Using the Angular tour of heroes tutorial.

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: Hero[];
+
+  selectedHero : Hero;
+
+  constructor(private heroService: HeroService) { // adding an access modifier automatically assigns that parameter to a field of the same name for this instance.
+    // this.heroService = HeroService;
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+}
