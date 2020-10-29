722db57dbcca940c379afea70acb8e6f2abdc040
Wenrui Xu <tigerwxu@gmail.com>
2020-10-30 10:21:42 +1300

    Made dashboard display 1st to 5th hero rather than 2nd to 5th

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+// Routes map paths to components
+// This allows angular to be "single-page", in that navigating to different paths
+// doesn't reset the state of the application/webpage as a whole.
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
